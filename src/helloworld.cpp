0097deccbabee3be9b9629301e9ad7416b56d781
Chris Lee <ctlee@ucsd.edu>
2020-03-29 17:27:50 -0700

    Check on fix for geometry-central CMake

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,11 +1,7 @@
 // #include <geometrycentral/surface/halfedge_mesh.h>
 
 #include <iostream>
-#include "ddg_solver.h"
-
 int main(){
-    ddg_solver ddg;
-    std::cout << ddg.addone(1) << std::endl;
 
     std::cout << "Hello World!";
     return 0;
