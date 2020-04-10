fbd6d146a9d7acdd0625f20b533b83674816f0ab
Cuncheng Zhu <cuzhu@ucsd.edu>
2020-04-09 22:47:57 -0700

    trying a lot of build in features of geometry central

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -30,7 +30,7 @@ std::ostream& operator<< (std::ostream& output, const std::vector<T>& v) {
 
 int main() {
 	
-
+//initialization of the geometry and mesh 
 	// Construct an icosahedron 
 	// Construct a std::vector of Vector 3
 	std::vector<Vector3> coords;
@@ -94,6 +94,20 @@ int main() {
 
 	// populate the quantity
 	geometry.requireFaceAreas();
+	geometry.requireCotanLaplacian();
+	geometry.requireVertexGalerkinMassMatrix();
+	geometry.requireVertexGaussianCurvatures();
+
+	Eigen::SparseMatrix<double> L = geometry.cotanLaplacian;
+	std::cout << Eigen::MatrixXd(L) << std::endl;
+
+	L = geometry.vertexGalerkinMassMatrix;
+	std::cout << Eigen::MatrixXd(L) << std::endl;
+
+	geosurf::VertexData<double> KG = geometry.vertexGaussianCurvatures;
+	Eigen::Matrix<double, Eigen::Dynamic, 1> v = KG.toVector();
+	std::cout << "Gaussian" << v << std::endl; 
+
 
 	for (geosurf::Face f : mesh->faces()) {
 
@@ -104,8 +118,20 @@ int main() {
 		// input data without touching caches.
 		// Generally discouraged but occasionally useful.
 		area = vpg->faceArea(f);
-		std::cout << area << std::endl;
+		//std::cout << area << std::endl;
 	}
+
+	// Compute vertex area
+	
+	geosurf::VertexData<double> vertArea(*mesh, 0.);
+	for (geosurf::Vertex v : mesh->vertices()) {
+		for (geosurf::Face f : v.adjacentFaces()) {
+			vertArea[v] += geometry.faceAreas[f] / f.degree();
+		}
+		//std::cout << "degree =" << f.degree() << std::endl;
+		std::cout << vertArea[v] << std::endl;
+	}
+
 
 	size_t a;
 	a = (mesh->nEdges());
@@ -113,10 +139,10 @@ int main() {
 	a = (mesh->nHalfedges());
 	std::cout << a << std::endl;
 
-	//using namespace geometrycentral::surface
-	polyscope::init();
+	/*polyscope::init();
 	polyscope::registerSurfaceMesh("myMesh", vpg->inputVertexPositions,mesh->getFaceVertexList());
-	polyscope::show();
+	polyscope::show();*/
+	
 
 	return 0;
 
@@ -124,95 +150,3 @@ int main() {
 }
 
 
-//template<typename T>
-//::std::ostream& operator<<(::std::ostream& output, const std::vector<T>& v) {
-//    output << "[";
-//    for (T item : v) {
-//        output << item << ", ";
-//    }
-//    output << "]";
-//    return output;
-//}
-//
-//
-//// strip unused vertices from face-vertex lists
-//void stripUnusedVertices(std::vector<Vector3>& positions, std::vector<std::vector<size_t>>& faceIndices) {
-//    size_t nVert = positions.size();
-//
-//
-//    // Find any unused vertices
-//    std::vector<size_t> vertexDegreeCount(nVert, 0);
-//    size_t nUsedVerts = 0;
-//    for (auto& f : faceIndices) {
-//        for (auto& i : f) {
-//            // Make sure we can safely index positions
-//            GC_SAFETY_ASSERT(i < positions.size(),
-//                "face index list has a vertex index which is greater than the number of vertices");
-//            vertexDegreeCount[i]++;
-//            if (vertexDegreeCount[i] == 1) {
-//                nUsedVerts++;
-//            }
-//        }
-//    }
-//
-//
-//    // Early exit if dense
-//    if (nUsedVerts == nVert) {
-//        return;
-//    }
-//
-//
-//    // Else: strip unused vertices and re-index faces
-//    size_t nNewVertices = 0;
-//    std::vector<size_t> oldToNewVertexInd(nVert);
-//    for (size_t iV = 0; iV < nVert; iV++) {
-//        if (vertexDegreeCount[iV] > 0) {
-//            oldToNewVertexInd[iV] = nNewVertices;
-//            positions[nNewVertices] = positions[iV];
-//            nNewVertices++;
-//        }
-//    }
-//    positions.resize(nNewVertices);
-//    for (auto& f : faceIndices) {
-//        for (auto& i : f) {
-//            i = oldToNewVertexInd[i];
-//        }
-//    }
-//}
-//
-//
-//
-//int main() {
-//    //std::unique_ptr<HalfedgeMesh> mesh = new HalfedgeMesh();
-//    // Construct an icosahedron
-//    // Construct a std::vector of Vector3
-//    std::vector<Vector3> coords;
-//    std::vector<std::vector<std::size_t>> polygons;
-//    // [[1,2,3], [2,3,4],[..] ]
-//    coords.push_back(Vector3{ 1.2f,0,0 });
-//    coords.push_back(Vector3{ -1,0,0 });
-//    coords.push_back(Vector3{ 0,1,0 });
-//    coords.push_back(Vector3{ 0,0,1 });
-//    for (Vector3 v : coords)
-//        std::cout << v << std::endl;
-//    polygons.push_back(std::vector<std::size_t>{0, 1, 2});
-//    polygons.push_back(std::vector<std::size_t>{0, 1, 3});
-//    polygons.push_back(std::vector<std::size_t>{0, 2, 3});
-//    polygons.push_back(std::vector<std::size_t>{1, 2, 3});
-//    for (std::vector<std::size_t> f : polygons)
-//        std::cout << f << std::endl;
-//    // std::cout << static_cast<std::size_t>(3.14159) << std::endl;
-//    // int i = std::pow(2,31)-1;
-//    // std::cout << i << ": " << std::bitset<32>(i) << std::endl;
-//    // std::cout << i+1 << ": " << std::bitset<32>(i+1) << std::endl;
-//    // std::size_t t = std::pow(2,31)-1;
-//    // std::cout << t << ": " << std::bitset<32>(t) << std::endl;
-//    // std::cout << t+1 << ": " << std::bitset<32>(t+1) << std::endl;
-//    PolygonSoupMesh soup(polygons, coords);
-//    soup.mergeIdenticalVertices();
-//    // stripUnusedVertices(soup.vertexCoordinates, soup.polygons);
-//    std::unique_ptr<HalfedgeMesh> mesh;
-//    std::unique_ptr<geometrycentral::surface::VertexPositionGeometry> vpg;
-//    std::tie(mesh, vpg) = geometrycentral::surface::makeHalfedgeAndGeometry(soup.polygons, soup.vertexCoordinates, true);
-//    return 0;
-//}
