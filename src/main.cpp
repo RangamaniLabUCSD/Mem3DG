91a572a4ef0ecf874829b292e5b18458cc4d94bd
Cuncheng Zhu <cuzhu@ucsd.edu>
2020-04-21 10:23:40 -0700

    contruct the class force

--- src/main.cpp
+++ src/main.cpp
@@ -39,64 +39,29 @@ int main() {
 	std::vector<gc::Vector3> coords;
 	std::vector<std::vector<std::size_t>> polygons;
 
-	icosphere(coords, polygons, 2);
+	icosphere(coords, polygons, 3);
 
 	gc::PolygonSoupMesh soup(polygons, coords);
 	soup.mergeIdenticalVertices();
 
-	std::unique_ptr<gcs::HalfedgeMesh> mesh;
-	std::unique_ptr<gcs::VertexPositionGeometry> vpg;
-	std::tie(mesh, vpg) = gcs::makeHalfedgeAndGeometry(soup.polygons, soup.vertexCoordinates, true);
+	std::unique_ptr<gcs::HalfedgeMesh> ptrmesh;
+	std::unique_ptr<gcs::VertexPositionGeometry> ptrvpg;
+	std::tie(ptrmesh, ptrvpg) = gcs::makeHalfedgeAndGeometry(soup.polygons, soup.vertexCoordinates, true);
 
-	/*Eigen::Matrix<double, Eigen::Dynamic, 3>& bf = bending_force(mesh,vpg,1.0,0);
-	std::cout << "bending force" << std::endl << bf << std::endl;*/
+	force f(*ptrmesh, *ptrvpg);
 
-	gcs::IntrinsicGeometryInterface& geometry = *vpg;
+	std::cout << "Sizeof Force: " << sizeof(force) << std::endl;
 
-	// try out some functionality of eigen
-	std::cout << "Eigen version:" << EIGEN_MAJOR_VERSION << "." << std::endl;
-	using namespace Eigen;
-	Matrix3f A;
-	Matrix3d B;
-
-	Matrix<double, 10, 10> M1;
-	A << 1.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f;
-
-	//std::cout << A << std::endl;
-	std::cout << "B's size " << B.size() << std::endl;
-	std::cout << "B's shape " << B.cols() << "x" << B.rows() << std::endl;
-
-	for (std::size_t j = 0; j < B.rows(); ++j) {
-		for (std::size_t i = 0; i < B.cols(); ++i) {
-			B(i, j) = 0.0;
-		}
+	//std::unique_ptr<force> ptrf(ptrmesh, ptrvpg);
+	Eigen::Matrix<double, Eigen::Dynamic, 3> bf = f.bending_force(1.0, 0);
+	std::cout << "bending force" << std::endl << bf << std::endl;
+	for (std::size_t i = 0; i < bf.rows(); ++i) {
+		std::cout << "force mag" <<  bf.row(i).norm() << std::endl;
 	}
-	//std::cout << B << std::endl;
 
-	// populate the quantity
-	geometry.requireFaceAreas();
-	geometry.requireCotanLaplacian();
-	geometry.requireVertexGalerkinMassMatrix();
-	geometry.requireVertexGaussianCurvatures();
+	gcs::IntrinsicGeometryInterface& geometry = *ptrvpg;
 
-	// weak(conformal) laplacian operator
-	// pass by reference to avoid copying 
-	Eigen::SparseMatrix<double>& L = geometry.cotanLaplacian;
-	//std::cout << Eigen::MatrixXd(L) << std::endl;
-
-	// Mass matrix (Galerkin approximation)
-	Eigen::SparseMatrix<double>& M = geometry.vertexGalerkinMassMatrix;
-	//std::cout << Eigen::MatrixXd(L) << std::endl;
-
-	// Gaussian curvature
-	// gcs::VertexData<double> KG = geometry.vertexGaussianCurvatures;
-	Eigen::Matrix<double, Eigen::Dynamic, 1> KG = geometry.vertexGaussianCurvatures.toVector();
-	// std::cout << "Gaussian" << KG << std::endl;
-
-
-	for (gcs::Face f : mesh->faces()) {
+	for (gcs::Face f : ptrmesh->faces()) {
 
 		// Managed array holding quantity
 		double area = geometry.faceAreas[f];
@@ -104,13 +69,13 @@ int main() {
 		// Immediate computation, computes directly from
 		// input data without touching caches.
 		// Generally discouraged but occasionally useful.
-		area = vpg->faceArea(f);
+		area = ptrvpg->faceArea(f);
 		//std::cout << area << std::endl;
 	}
 
 	// Compute vertex area
-	gcs::VertexData<double> vertArea(*mesh, 0.);
-	for (gcs::Vertex v : mesh->vertices()) {
+	gcs::VertexData<double> vertArea(*ptrmesh, 0.);
+	for (gcs::Vertex v : ptrmesh->vertices()) {
 		for (gcs::Face f : v.adjacentFaces()) {
 			vertArea[v] += geometry.faceAreas[f] / f.degree();
 		}
@@ -119,35 +84,35 @@ int main() {
 	}
 
 	/*
-	// print properties of mesh
+	// print properties of ptrmesh
 	size_t a;
-	a = (mesh->nEdges());
+	a = (ptrmesh->nEdges());
 	std::cout << a << std::endl;
-	a = (mesh->nHalfedges());
+	a = (ptrmesh->nHalfedges());
 	std::cout << a << std::endl;
 	*/
 
 
 	/// Explicit COPY based approach
-	// gcs::VertexData<Vector3>& ivp = vpg->inputVertexPositions;
-	// Eigen::Matrix<gc::Vector3, Eigen::Dynamic, 1> eivp = vpg->inputVertexPositions.toVector();
+	// gcs::VertexData<Vector3>& ivp = ptrvpg->inputVertexPositions;
+	// Eigen::Matrix<gc::Vector3, Eigen::Dynamic, 1> eivp = ptrvpg->inputVertexPositions.toVector();
 
 	// //manipulate eivp
 	// // ...
 	// //then copy it back
-	// vpg->fromVector(eivp);
+	// ptrvpg->fromVector(eivp);
 
 	// std::cout << "EIVP: " << eivp << std::endl;
 
 
 	/// Compiler error no access to protected member object.
-	// auto & bar = vpg->inputVertexPositions.data;
+	// auto & bar = ptrvpg->inputVertexPositions.data;
 
 
 	/// CAN WE AVOID THE COPY?
 	// .rawdata() is the function that we added to access the protected member "data" 
 	// vector of vector3 
-	std::vector<gc::Vector3, Eigen::aligned_allocator<gc::Vector3>>& foo = vpg->inputVertexPositions.rawdata();
+	std::vector<gc::Vector3, Eigen::aligned_allocator<gc::Vector3>>& foo = ptrvpg->inputVertexPositions.rawdata();
 	for (int i = 0; i < foo.size(); ++i) {
 		std::cout << "Foooo[" << i << "]: " << foo[i] << std::endl;
 
@@ -156,7 +121,7 @@ int main() {
 	// [x,y,z;x,y,z;x,y,z;]
 
 	// .data() returns a pointer to the first element in the array used internally by the vector.
-	gc::Vector3* d = vpg->inputVertexPositions.rawdata().data();
+	gc::Vector3* d = ptrvpg->inputVertexPositions.rawdata().data();
 
 	Eigen::Map<Eigen::Matrix<gc::Vector3, Eigen::Dynamic, 1>> evec3(d, foo.size());
 	
@@ -180,14 +145,14 @@ int main() {
 	 	v[2] *= 1;
 
 	 	std::cout << "bfr: " << v << std::endl;
-	 	std::cout << "cmp: " << vpg->inputVertexPositions[i] << std::endl;
+	 	std::cout << "cmp: " << ptrvpg->inputVertexPositions[i] << std::endl;
 	 	++i;
 	 }
 
 	 /*polyscope::init();
-	 polyscope::registerSurfaceMesh("myMesh",
-	 							   vpg->inputVertexPositions,
-	 							   mesh->getFaceVertexList());
+	 polyscope::registerSurfacemesh("myMesh",
+	 							   ptrvpg->inputVertexPositions,
+	 							   ptrmesh->getFaceVertexList());
 	 polyscope::show();*/
 
 	return 0;
