22a6ad1ff3578f7cf5ece1c56d73586af8a47992
Cuncheng Zhu <cuzhu@ucsd.edu>
2020-04-21 10:23:40 -0700

    contruct the class force

--- include/ddgsolver/force.h
+++ include/ddgsolver/force.h
@@ -3,11 +3,37 @@
 #include <geometrycentral/surface/intrinsic_geometry_interface.h>
 #include <geometrycentral/surface/halfedge_mesh.h>
 #include <geometrycentral/surface/vertex_position_geometry.h>
+#include <Eigen/Core>
+#include <Eigen/SparseLU>
 
-using HalfedgeMesh = geometrycentral::surface::HalfedgeMesh;
+namespace gc = ::geometrycentral;
+namespace gcs = ::geometrycentral::surface;
 
-Eigen::Matrix<double, Eigen::Dynamic, 3> bending_force(std::unique_ptr<HalfedgeMesh>& mesh,
-    std::unique_ptr<geometrycentral::surface::VertexPositionGeometry>& vpg, double Kb, double H0);
+
+class force{
+public:
+    gcs::HalfedgeMesh& mesh;
+    gcs::VertexPositionGeometry& vpg;
+
+    Eigen::SparseMatrix<double> M;
+    Eigen::SparseMatrix<double> M_inv;
+
+    force(gcs::HalfedgeMesh& mesh_, gcs::VertexPositionGeometry& vpg_): mesh(mesh_), vpg(vpg_) {
+        vpg.requireVertexGalerkinMassMatrix();
+        M = vpg.vertexGalerkinMassMatrix;
+
+        Eigen::SparseLU<Eigen::SparseMatrix<double> > solver;
+        solver.compute(M);
+        std::size_t n = mesh.nVertices();
+        Eigen::SparseMatrix<double> I(n, n);
+        I.setIdentity();
+        M_inv = solver.solve(I);
+    }
+
+    Eigen::Matrix<double, Eigen::Dynamic, 3> bending_force(double Kb, double H0);
+
+    Eigen::Matrix<double, Eigen::Dynamic, 3> bending_force(double Kb, Eigen::Matrix<double, Eigen::Dynamic, 1> H0);
+};
 
 /*
 Eigen::Matrix<double, Eigen::Dynamic, 1> stretching_force(std::unique_ptr<HalfedgeMesh> mesh,
--- src/force.cpp
+++ src/force.cpp
@@ -6,36 +6,33 @@
 #include <geometrycentral/surface/vertex_position_geometry.h>
 #include "geometrycentral/utilities/vector3.h"
 #include <iostream>
-#include<Eigen/IterativeLinearSolvers>
+#include <Eigen/IterativeLinearSolvers>
 #include "geometrycentral/numerical/linear_solvers.h"
 
-using namespace Eigen;
-using Vector3 = geometrycentral::Vector3;
-namespace gc = ::geometrycentral;
+
+namespace gc  = ::geometrycentral;
 namespace gcs = ::geometrycentral::surface;
 
-Eigen::Matrix<double, Eigen::Dynamic, 3> bending_force(std::unique_ptr<HalfedgeMesh>& mesh,
-    std::unique_ptr<geometrycentral::surface::VertexPositionGeometry>& vpg, double Kb, double H0) {
+Eigen::Matrix<double, Eigen::Dynamic, 3> force::bending_force(double Kb, double H0) {
 
+    //gcs::IntrinsicGeometryInterface& geometry = vpg;
 
-    gcs::IntrinsicGeometryInterface& geometry = *vpg;
+    vpg.requireCotanLaplacian();
+    //vpg.requireVertexGalerkinMassMatrix();
+    vpg.requireVertexGaussianCurvatures();
 
-    geometry.requireCotanLaplacian();
-    geometry.requireVertexGalerkinMassMatrix();
-    geometry.requireVertexGaussianCurvatures();
+    // weak(conformal) laplacian operator
+    Eigen::SparseMatrix<double>& L = vpg.cotanLaplacian;
+    //std::cout << Eigen::MatrixXd(L) << std::endl;
 
-	// weak(conformal) laplacian operator
-	Eigen::SparseMatrix<double>& L = geometry.cotanLaplacian;
-	//std::cout << Eigen::MatrixXd(L) << std::endl;
+    // Mass matrix (Galerkin approximation)
+    //Eigen::SparseMatrix<double>& M = vpg.vertexGalerkinMassMatrix;
+    //std::cout << "mass" << Eigen::MatrixXd(M).inverse() << std::endl;
 
-	// Mass matrix (Galerkin approximation)
-	Eigen::SparseMatrix<double>& M = geometry.vertexGalerkinMassMatrix;
-	//std::cout << "mass" << Eigen::MatrixXd(M).inverse() << std::endl;
-
-	// Gaussian curvature
-	gcs::VertexData<double>& gaussian = geometry.vertexGaussianCurvatures;
-	Eigen::Matrix<double, Eigen::Dynamic, 1> KG = gaussian.toVector();
-	//std::cout << "Gaussian" << KG << std::endl;
+    // Gaussian curvature
+    gcs::VertexData<double>& gaussian = vpg.vertexGaussianCurvatures;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> KG = gaussian.toVector();
+    //std::cout << "Gaussian" << KG << std::endl;
 
     ////std::cout << "force cpp, KG size:  " << KG.size() << std::endl;
     //std::vector<std::vector<std::size_t>>& face_list = mesh->getFaceVertexList();
@@ -45,7 +42,6 @@ Eigen::Matrix<double, Eigen::Dynamic, 3> bending_force(std::unique_ptr<HalfedgeM
 
     //Eigen::Matrix<double, Eigen::Dynamic, 3> vl;
     //vl.resize(n_vertices,3)
-
     ////std::cout << "1,2,: "<< vertex_list[5][2] << std::endl;
 
     //for (int col = 0; col < 3; ++col) {
@@ -55,37 +51,33 @@ Eigen::Matrix<double, Eigen::Dynamic, 3> bending_force(std::unique_ptr<HalfedgeM
     //    }
     //}
 
-    size_t n_vertices = (mesh->nVertices());
-    gc::Vector3* d = vpg->inputVertexPositions.rawdata().data();
-    Eigen::Map<Eigen::Matrix<gc::Vector3, Eigen::Dynamic, 1>> evec3(d, n_vertices);
-    Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, 3>> evecdouble(reinterpret_cast<double*>(d), n_vertices, 3);
-    //std::cout << MatrixXd(M).inverse() << std::endl;
-    auto Hn = MatrixXd(M).inverse() * MatrixXd(L) * evecdouble/2;
-    Matrix<double, Dynamic, 1> H;                     
-    Matrix<double, Dynamic, 3> n;
+    size_t n_vertices = (mesh.nVertices());
+    gc::Vector3* d = vpg.inputVertexPositions.rawdata().data();
+
+    Eigen::Map<Eigen::Matrix<double, 3, Eigen::Dynamic>> positions(reinterpret_cast<double*>(d), 3, n_vertices);
+    //std::cout << "evecdouble" << evecdouble.cols() << std::endl;
+    Eigen::Matrix<double, Eigen::Dynamic, 3> Hn = M_inv * L * positions.transpose() / 2;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> H;
+    Eigen::Matrix<double, Eigen::Dynamic, 3> n;
     H.resize(n_vertices, 1);
     n.resize(n_vertices, 3);
 
 
     for (int row = 0; row < n_vertices; ++row) {
         H(row) = Hn.row(row).norm();
+        std::cout << "curvature" << H(row) << std::endl;
         n.row(row) = Hn.row(row) / H(row);
     }
 
-
-    //std::cout << n << std::endl;
-
-    auto lap_H = MatrixXd(M).inverse() * MatrixXd(L) * H;
-    std::cout << (H - MatrixXd::Constant(n_vertices, 1, H0)).array() * square(H.array()) << std::endl;
-    auto f_mag = M * (-2 * Kb * (2 * ((H - MatrixXd::Constant(162,1,H0)).array() * square(H.array())).matrix() + H0 * H - KG) + lap_H);
-    Matrix<double, Dynamic, 3> f;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> lap_H = M_inv * L * H;
+    //std::cout << (H - MatrixXd::Constant(n_vertices, 1, H0)).array() * square(H.array()) << std::endl;
+    auto f_mag = M * (-2 * Kb * (2 * ((H - Eigen::MatrixXd::Constant(n_vertices, 1, H0)).array() * square(H.array())).matrix() + H0 * H - KG) + lap_H);
+    Eigen::Matrix<double, Eigen::Dynamic, 3> f;
     f.resize(n_vertices, 3);
 
-    for (size_t row = 0; row < mesh->nVertices(); ++row) {
+    for (size_t row = 0; row < mesh.nVertices(); ++row) {
         f.row(row) = f_mag(row) * n.row(row);
     }
 
-    return n;
-
+    return f;
 }
-
