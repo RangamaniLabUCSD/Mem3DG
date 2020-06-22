417303d2728f013d3bd8f717b367f36fa206d23c
Cuncheng Zhu <cuzhu@ucsd.edu>
2020-06-22 10:14:48 -0700

    external force in same direction

--- src/external_force.cpp
+++ src/external_force.cpp
@@ -13,7 +13,8 @@ namespace ddgsolver {
 		// alias distance
 		auto externalForce_e = ddgsolver::EigenMap<double, 3>(externalForces);
 		// std::cout << "external force::" << appliedForceMagnitude << std::endl;
-		externalForce_e = vertexAreaGradientNormal.array().colwise() *
-			appliedForceMagnitude.array();
+		// externalForce_e = vertexAreaGradientNormal.array().colwise() *
+		//	appliedForceMagnitude.array();
+		externalForce_e = appliedForceMagnitude * vertexAreaGradientNormal.row(ptInd);
 	}
 }
