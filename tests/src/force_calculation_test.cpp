f7075c2262465f9062a1f9418c386824f2a4991d
Cuncheng Zhu <cuzhu@ucsd.edu>
2020-06-23 16:24:06 -0700

    add test for force calculation

--- tests/src/force_calculation_test.cpp
+++ tests/src/force_calculation_test.cpp
@@ -11,6 +11,8 @@
 #include <geometrycentral/surface/ply_halfedge_mesh_data.h>
 #include <geometrycentral/utilities/vector3.h>
 
+#include <Eigen/Core>
+
 #include "ddgsolver/force.h"
 #include "ddgsolver/icosphere.h"
 #include "ddgsolver/util.h"
@@ -31,12 +33,12 @@ protected:
     /// physical parameters 
     p.Kb = 0.02;			//Kb
     p.H0 = 2;				//H0
-    p.Kse = 0;      //Kse
+    p.Kse = 1;      //Kse
     p.Ksl = 3;				//Ksl
-    p.Ksg = 0;				//Ksg
-    p.Kv = 1;			  //Kv
+    p.Ksg = 1;				//Ksg
+    p.Kv = 2;			  //Kv
     p.gamma = 1;				//gamma
-    p.Vt = 1 * 0.73;			//Vt
+    p.Vt = 1 * 0.5;			//Vt
     p.kt = 0.0001;		//Kt 
     p.ptInd = 1;
     p.extF = 2 * 0;
@@ -54,18 +56,106 @@ protected:
   }
 };
 
-TEST_F(ForceCalculationTest, ConservativeForceTest) {
+TEST_F(ForceCalculationTest, bendingForceTest) {
   auto& mesh = *ptrmesh;
   auto& vpg = *ptrvpg;
   ddgsolver::Force f(mesh, vpg, p);
+
   f.getConservativeForces();
-  auto conservativeForce1 = ddgsolver::EigenMap<double, 3>(f.bendingForces) +
-    ddgsolver::EigenMap<double, 3>(f.stretchingForces) +
-    ddgsolver::EigenMap<double, 3>(f.pressureForces);
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> bendingForces1 
+    = ddgsolver::EigenMap<double, 3>(f.bendingForces);
+
   f.getConservativeForces();
-  auto conservativeForce2 = ddgsolver::EigenMap<double, 3>(f.bendingForces) +
-    ddgsolver::EigenMap<double, 3>(f.stretchingForces) +
-    ddgsolver::EigenMap<double, 3>(f.pressureForces);
-  ASSERT_EQ(conservativeForce1, conservativeForce2);
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> bendingForces2
+    = ddgsolver::EigenMap<double, 3>(f.bendingForces);
+  ASSERT_EQ(bendingForces1, bendingForces2);
 };
+
+TEST_F(ForceCalculationTest, pressureForceTest) {
+  auto& mesh = *ptrmesh;
+  auto& vpg = *ptrvpg;
+  ddgsolver::Force f(mesh, vpg, p);
+
+  f.getConservativeForces();
+  /*Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> pressureForces1 
+    = ddgsolver::EigenMap<double, 3>(f.pressureForces);*/
+
+  auto pressureForces1 = ddgsolver::EigenMap<double, 3>(f.pressureForces);
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> a(pressureForces1);
+  a << pressureForces1;
+
+  f.getConservativeForces();
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> pressureForces2 
+    = ddgsolver::EigenMap<double, 3>(f.pressureForces);
+  ASSERT_EQ(pressureForces1, pressureForces2);
+};
+
+TEST_F(ForceCalculationTest, stretchingForceTest) {
+  auto& mesh = *ptrmesh;
+  auto& vpg = *ptrvpg;
+  ddgsolver::Force f(mesh, vpg, p);
+
+  f.getConservativeForces();
+  
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> stretchingForces1 
+    = ddgsolver::EigenMap<double, 3>(f.stretchingForces);
+
+  f.getConservativeForces();
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> stretchingForces2 
+    = ddgsolver::EigenMap<double, 3>(f.stretchingForces);
+
+  ASSERT_EQ(stretchingForces1, stretchingForces2);
+};
+
+TEST_F(ForceCalculationTest, onePassVsIndividualBendingTest) {
+  auto& mesh = *ptrmesh;
+  auto& vpg = *ptrvpg;
+  ddgsolver::Force f(mesh, vpg, p);
+
+  f.getConservativeForces();
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> bendingForces1
+    = ddgsolver::EigenMap<double, 3>(f.bendingForces);
+
+  f.getBendingForces();
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> bendingForces2
+    = ddgsolver::EigenMap<double, 3>(f.bendingForces);
+  ASSERT_EQ(bendingForces1, bendingForces2);
+};
+
+TEST_F(ForceCalculationTest, onePassVsIndividualPressureTest) {
+  auto& mesh = *ptrmesh;
+  auto& vpg = *ptrvpg;
+  ddgsolver::Force f(mesh, vpg, p);
+
+  f.getConservativeForces();
+  /*Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> pressureForces1
+    = ddgsolver::EigenMap<double, 3>(f.pressureForces);*/
+
+  auto pressureForces1 = ddgsolver::EigenMap<double, 3>(f.pressureForces);
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> a(pressureForces1);
+  a << pressureForces1;
+
+  f.getPressureForces();
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> pressureForces2
+    = ddgsolver::EigenMap<double, 3>(f.pressureForces);
+  ASSERT_EQ(pressureForces1, pressureForces2);
+};
+
+TEST_F(ForceCalculationTest, onePassVsIndividualStretchingTest) {
+  auto& mesh = *ptrmesh;
+  auto& vpg = *ptrvpg;
+  ddgsolver::Force f(mesh, vpg, p);
+
+  f.getConservativeForces();
+
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> stretchingForces1
+    = ddgsolver::EigenMap<double, 3>(f.stretchingForces);
+
+  f.getStretchingForces();
+  Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> stretchingForces2
+    = ddgsolver::EigenMap<double, 3>(f.stretchingForces);
+
+  ASSERT_EQ(stretchingForces1, stretchingForces2);
+};
+
 }
