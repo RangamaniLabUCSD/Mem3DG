d4005f2fcd35b8845cf710740bc87ed7d7b2a419
Cuncheng Zhu <cuzhu@ucsd.edu>
2020-07-12 16:56:12 -0700

    argparse, clean up

--- python/pyddg/main.py
+++ python/pyddg/main.py
@@ -1,19 +1,27 @@
 import sys
 import json
 import os
+import argparse
 
-with open("config.json") as f:
+# parse the config file 
+parser = argparse.ArgumentParser()
+parser.add_argument("config", help = "configuration file (.json) used for the simulation", type = str)
+args = parser.parse_args()
+
+# read the config file
+with open(args.config) as f:
     data = json.load(f)
-
 geo = data["parameters"]["geometry"]
 prop = data["parameters"]["properties"]
 var = data["parameters"]["variables"]
 inte = data["parameters"]["integration"]
 dep = data["parameters"]["dependencies"]
 
+# find dependency
 sys.path.append(dep["pyddg"])
 import pyddg
 
+# make I/O directories
 cwd = os.getcwd()
 IDir = os.path.join(cwd, os.path.split(geo["inputMesh"])[0])
 ODir = os.path.join(cwd, geo["outputDir"])
@@ -22,12 +30,13 @@ if not os.path.exists(IDir):
 if not os.path.exists(ODir):
     os.mkdir(ODir)
 
+# create starting mesh
 pyddg.genIcosphere( nSub = geo["nSub"] 
                     , path = geo["inputMesh"])
-
 # pyddg.genUVsphere( nSub = geo["nSub"] 
 #                    , path = geo["inputMesh"])
 
+# run simulation
 pyddg.driver(inputMesh = geo["inputMesh"],
             outputDir = geo["outputDir"],
 
