0acd09115e91c0a590b936a73d3e289e0445507c
Chris Lee <ctlee@ucsd.edu>
2020-10-07 11:29:47 -0700

    Fix syntax
    Signed-off-by: Chris Lee <ctlee@ucsd.edu>

--- cmake/FindnetCDF.cmake
+++ cmake/FindnetCDF.cmake
@@ -23,7 +23,7 @@ if (netCDF_FOUND)
 
       # temporary hack to workaround netcdf packaging in conda
       get_target_property(_libs netCDF::netcdf INTERFACE_LINK_LIBRARIES)
-      if(${_libs})
+      if(_libs)
         list(FILTER _libs EXCLUDE REGEX ".*/x86_64-conda_cos6-linux-gnu/.*")
         set_target_properties(netCDF::netcdf PROPERTIES INTERFACE_LINK_LIBRARIES "${_libs}")
       endif ()
