05400da2253072dc3b1aa45e261ba8c517018c85
cuzhucuncheng <cuzhu@ucsd.edu>
2020-11-04 16:16:59 -0800

    not include masked (contrained v) in energy

--- src/bending_energy.cpp
+++ src/bending_energy.cpp
@@ -17,8 +17,8 @@
 
 #include <geometrycentral/surface/halfedge_mesh.h>
 #include <geometrycentral/surface/vertex_position_geometry.h>
-#include <geometrycentral/utilities/vector3.h>
 #include <geometrycentral/utilities/eigen_interop_helpers.h>
+#include <geometrycentral/utilities/vector3.h>
 
 #include <Eigen/Core>
 
@@ -30,16 +30,16 @@ namespace integration {
 namespace gc = ::geometrycentral;
 namespace gcs = ::geometrycentral::surface;
 
-double getL2ErrorNorm(
-    Eigen::SparseMatrix<double> M,
-    Eigen::Matrix<double, Eigen::Dynamic, 3>
-        physicalPressure){
+double
+getL2ErrorNorm(Eigen::SparseMatrix<double> M,
+               Eigen::Matrix<double, Eigen::Dynamic, 3> physicalPressure) {
 
   return sqrt(
       (M * rowwiseDotProduct(physicalPressure, physicalPressure)).sum());
 }
 
-std::tuple<double, double, double, double, double, double> getFreeEnergy(Force &f) {
+std::tuple<double, double, double, double, double, double>
+getFreeEnergy(Force &f) {
   // comment: this may not be useful, the convergence can be be tested by
   // checking its derivative which is the forces excluding the DPD forces. The
   // energy trajectory of could actually numerically integrated by post
@@ -49,15 +49,15 @@ std::tuple<double, double, double, double, double, double> getFreeEnergy(Force &
   // auto A = f.L.transpose() * f.M_inv * f.L; //could further cached since the
   // same for all time pt for (size_t i = 0; i < positions.rows(); i++) {
   // for
-  //(size_t j = 0; j < positions.rows(); j++) { 		Eb += positions.row(i) *
-  //A * positions.transpose().col(j);
+  //(size_t j = 0; j < positions.rows(); j++) { 		Eb += positions.row(i)
+  //* A * positions.transpose().col(j);
   //	}
   //}
 
   double bE;
   double sE;
   double pE;
-  
+
   double kE;
   double cE = 0;
   double totalE;
@@ -68,7 +68,7 @@ std::tuple<double, double, double, double, double, double> getFreeEnergy(Force &
     double A_difference = f.surfaceArea - f.targetSurfaceArea;
     double V_difference = f.volume - f.refVolume * f.P.Vt;
 
-    bE = (f.P.Kb * f.M * (H_difference.array() * H_difference.array()).matrix())
+    bE = (f.P.Kb * f.M * (f.mask.cast<double>().array() * H_difference.array() * H_difference.array()).matrix())
              .sum();
     sE = f.P.Ksg * A_difference;
     pE = -f.P.Kv * V_difference;
@@ -88,8 +88,8 @@ std::tuple<double, double, double, double, double, double> getFreeEnergy(Force &
     double A_difference = f.surfaceArea - f.targetSurfaceArea;
     double V_difference = f.volume - f.refVolume * f.P.Vt;
 
-    bE = (f.P.Kb * f.M * (H_difference.array() * H_difference.array()).matrix())
-              .sum();
+    bE = (f.P.Kb * f.M * (f.mask.cast<double>().array() * H_difference.array() * H_difference.array()).matrix())
+             .sum();
     sE = f.P.Ksg * A_difference * A_difference / f.targetSurfaceArea / 2;
     pE = f.P.Kv * V_difference * V_difference / (f.refVolume * f.P.Vt) / 2;
 
@@ -97,14 +97,15 @@ std::tuple<double, double, double, double, double, double> getFreeEnergy(Force &
     kE = 0.5 * (f.M * (vel.array() * vel.array()).matrix()).sum();
 
     if (f.isProtein) {
-      double cE = (f.M * f.P.epsilon * f.proteinDensity.raw()).sum(); 
+      double cE = (f.M * f.P.epsilon * f.proteinDensity.raw()).sum();
     }
 
     totalE = bE + sE + pE + kE + cE;
   }
 
-  std::tuple<double, double, double, double, double, double> output(totalE, bE, sE, pE, kE, cE);
-  
+  std::tuple<double, double, double, double, double, double> output(
+      totalE, bE, sE, pE, kE, cE);
+
   return output;
 }
 } // namespace integration
