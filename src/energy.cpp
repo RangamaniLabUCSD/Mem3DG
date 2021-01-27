43daadc19ed0166e11ad6617b89b9f0e8ec6534d
cuncheng zhu <zhucuncheng@gmail.com>
2021-01-27 15:55:30 -0800

    pressure norm and nc continuation option

--- src/energy.cpp
+++ src/energy.cpp
@@ -33,7 +33,8 @@ void System::getBendingEnergy() {
   E.BE = P.Kb * H_difference.transpose() * M * H_difference;
 
   // when considering topological changes, additional term of gauss curvature
-  // E.BE = P.Kb * H_difference.transpose() * M * H_difference + P.KG * (M * vpg.vertexGaussianCurvatures.raw()).sum();
+  // E.BE = P.Kb * H_difference.transpose() * M * H_difference + P.KG * (M *
+  // vpg.vertexGaussianCurvatures.raw()).sum();
 }
 
 void System::getSurfaceEnergy() {
@@ -105,7 +106,7 @@ void System::getPotentialEnergy() {
 void System::getFreeEnergy() {
   // zero all energy
   E = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-
+  
   getKineticEnergy();
   getPotentialEnergy();
   E.totalE = E.kE + E.potE;
@@ -115,4 +116,10 @@ void System::getL2ErrorNorm(Eigen::Matrix<double, Eigen::Dynamic, 3> pressure) {
   L2ErrorNorm =
       sqrt((M * rowwiseDotProduct(pressure, pressure)).sum() / surfaceArea);
 }
+
+double System::getL2Norm(gcs::VertexData<gc::Vector3> pressure_data) const {
+  auto pressure = gc::EigenMap<double, 3>(pressure_data);
+  return sqrt((M * rowwiseDotProduct(pressure, pressure)).sum() / surfaceArea);
+}
+
 } // namespace mem3dg
