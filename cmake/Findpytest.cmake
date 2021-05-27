a3a98dab66f5043921c7fdbe8ed61ad619413b83
Chris Lee <ctlee@ucsd.edu>
2021-05-26 18:35:16 -0700

    Add python test in ctest
    Signed-off-by: Chris Lee <ctlee@ucsd.edu>

---
+++ cmake/Findpytest.cmake
@@ -0,0 +1,14 @@
+
+find_package(Python QUIET COMPONENTS Interpreter)
+if(Python_Interpreter_FOUND)
+    execute_process(COMMAND ${Python_EXECUTABLE} -c "from importlib import util;print(util.find_spec('pytest') is not None)"
+                    OUTPUT_VARIABLE PYTEST_IMPORT_STATUS
+                    ERROR_QUIET
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif(Python_Interpreter_FOUND)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(pytest DEFAULT_MSG
+    PYTEST_IMPORT_STATUS
+)
