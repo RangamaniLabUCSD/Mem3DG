4d51bb8d12e4fc0de976cc7e47bba30216b4d962
cuncheng zhu <zhucuncheng@gmail.com>
2022-01-09 11:56:01 -0800

    issue: segmentation fault on cortex

--- tests/scratch.cpp
+++ tests/scratch.cpp
@@ -29,42 +29,42 @@ int main() {
   // Eigen::Matrix<double, Eigen::Dynamic, 3> vpg;
   mem3dg::solver::Parameters p;
   // std::tie(mesh, vpg) = mem3dg::getCylinderMatrix(1, 16, 60, 7.5, 0);
-  std::string inputMesh =
-      "/home/cuzhu/attractive_repulsive_pearling/results/temp/frame24.ply";
+  std::string inputMesh = "/home/cuzhu/Mem3DG/tests/frame20.ply";
 
   /// physical parameters
   p.proteinMobility = 10;
   p.temperature = 0;
 
-  p.point.pt.resize(3);
+  p.point.pt.resize(2);
   p.point.pt << 0, 0;
   p.point.isFloatVertex = false;
-  
+
+  p.proteinDistribution.profile = "none";
   p.proteinDistribution.protein0.resize(1);
-  p.proteinDistribution.protein0 << -1;
+  p.proteinDistribution.protein0 << 0.5;
   p.proteinDistribution.lambdaPhi = 1e-7;
-  
+
   p.boundary.shapeBoundaryCondition = "fixed";
   p.boundary.proteinBoundaryCondition = "pin";
-  
+
   p.variation.isProteinVariation = true;
   p.variation.isShapeVariation = true;
   p.variation.radius = -1;
-  
+
   p.bending.Kb = 8.22e-5;
   p.bending.Kbc = 2 * 8.22e-5;
   p.bending.H0c = -60;
-  
+
   p.tension.isConstantSurfaceTension = false;
   p.tension.Ksg = 1;
   p.tension.A_res = 0;
   p.tension.At = 3.40904;
   p.tension.lambdaSG = 0;
-  
+
   p.adsorption.epsilon = -1e-4;
-  
+
   p.aggregation.chi = -2e-1;
-  
+
   p.osmotic.isPreferredVolume = false;
   p.osmotic.isConstantOsmoticPressure = true;
   p.osmotic.Kv = 0;
@@ -73,12 +73,13 @@ int main() {
   p.osmotic.Vt = -1;
   p.osmotic.cam = -1;
   p.osmotic.lambdaV = 0;
-  
+
   p.dirichlet.eta = 0.00005;
-  
-  p.selfAvoidance.d = 0.0001;
-  p.selfAvoidance.mu = 1e-8;
-  
+
+  p.selfAvoidance.d = 0.01;
+  p.selfAvoidance.mu = 1e-6;
+  p.selfAvoidance.p = 0.1;
+
   p.dpd.gamma = 0;
   p.external.Kf = 0;
 
@@ -93,7 +94,7 @@ int main() {
   mP.meshMutator.collapseSkinny = true;
 
   // mem3dg::solver::System system(mesh, vpg, p, mP, 0);
-  mem3dg::solver::System system(inputMesh, p, mP, 0, true);
+  mem3dg::solver::System system(inputMesh, p, mP, 0, false);
 
   const double dt = 0.01, T = 1000000, eps = 1e-4, tSave = 2, verbosity = 5;
   const std::string outputDir = "/tmp";
