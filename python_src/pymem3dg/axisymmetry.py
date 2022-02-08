1d00d2597648d1c8c8d0f84664e1ffb14529965c
cuncheng zhu <zhucuncheng@gmail.com>
2022-02-07 20:05:34 -0800

    push for local machine

--- python_src/pymem3dg/axisymmetry.py
+++ python_src/pymem3dg/axisymmetry.py
@@ -1,6 +1,6 @@
 import autograd.numpy as np
 from autograd import elementwise_grad as egrad
-from autograd import grad 
+from autograd import grad
 from ._core import *
 
 
@@ -17,6 +17,7 @@ class Parameters:
 
 
 class Energy:
+    totalEnergy = 0
     bendingEnergy = 0
     surfaceEnergy = 0
     pressureEnergy = 0
@@ -60,43 +61,19 @@ class System:
         self.radius_v = radius
         self.height_v = height
         self.parameter = parameter
-        self.populate_geometric_measurements(self.radius_v, self.height_v)
+        self.compute_totalEnergy(self.join(self.radius_v, self.height_v))
 
-    # def compute_meanCurvature(self, radius):
-    #     n = radius.shape[0]
+    def split(self, radius_height):
+        twiceN = radius_height.shape[0]
+        radius = radius_height[:int(twiceN/2)]
+        height = radius_height[int(twiceN/2):]
+        return radius, height
 
-    #     dr = np.diff(radius)
+    def join(self, radius, height):
+        return np.append(radius, height)
 
-    #     return np.sum(dr) 
-    def compute_meanCurvature(self, radius, height):
-        n = radius.shape[0]
-
-        dr = np.diff(radius)
-        dh = np.diff(height)
-        ds = np.sqrt(dr**2 + dh**2)
-
-        s1 = np.append(ds, 0)
-        s2 = np.append(0, ds)
-        self.arclength_v = (s1 + s2) * 0.5
-
-        psi = np.arctan2(dh, dr)
-
-        phi = np.diff(psi)
-        phi = np.append(0, phi)
-        phi = np.append(phi, 0)
-        self.turningAngle_v = phi
-
-        self.axialCurvature_v = self.turningAngle_v / self.arclength_v
-
-        radius_e = (radius[0:n-1] + radius[1:n]) * 0.5
-        radialCurvature_e = np.sin(psi) / radius_e * ds
-        rc1 = np.append(radialCurvature_e, 0)
-        rc2 = np.append(0, radialCurvature_e)
-        self.radialCurvature_v = (rc1 + rc2) * 0.5 / self.arclength_v
-
-        return np.sum(self.radialCurvature_v + self.axialCurvature_v)
-
-    def populate_geometric_measurements(self, radius, height):
+    def compute_totalEnergy(self, radius_height):
+        radius, height = self.split(radius_height)
         n = radius.shape[0]
 
         dr = np.diff(radius)
@@ -123,15 +100,15 @@ class System:
 
         self.meanCurvature_v = self.radialCurvature_v + self.axialCurvature_v
 
-    def compute_bendingEnergy(self):
-        self.energy.bendingEnergy = np.sum(
-            self.meanCurvature_v * self.meanCurvature_v * self.arclength_v)
+        self.energy.bendingEnergy = np.sum(self.meanCurvature_v * self.meanCurvature_v * self.arclength_v)
+
+        return self.energy.bendingEnergy
 
     def compute_bendingForce(self):
         # self.energy.bendingForce = egrad(
         #     self.compute_meanCurvature)(self.radius_v, self.height_v)
-        gradient = grad(self.compute_meanCurvature)
-        print(gradient(self.radius_v).shape)
-        self.energy.bendingForce = gradient(self.radius_v, self.height_v)
+        gradient = grad(self.compute_totalEnergy)
+        # print(gradient(np.append(self.radius_v, self.height_v)).shape)
+        self.energy.bendingForce = gradient(self.join(self.radius_v, self.height_v))
         # self.energy.bendingForce = egrad(
         #     self.compute_meanCurvature)(self.radius_v)
