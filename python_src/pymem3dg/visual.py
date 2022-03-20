e596eb15e9ab6e69e7dc4f542966b3488ffba3ed
cuzhucuncheng <cuzhu@ucsd.edu>
2022-03-19 17:30:17 -0700

    feat: init implement python animation

--- python_src/pymem3dg/visual.py
+++ python_src/pymem3dg/visual.py
@@ -1,6 +1,11 @@
-import matplotlib.pyplot as plt
-import pymem3dg as dg
 import numpy as np
+import matplotlib.pyplot as plt
+
+import pymem3dg as dg
+import pymem3dg.read as dg_read
+
+import polyscope as ps
+import polyscope.imgui as psim
 
 
 def matplotlibStyle(small=6, medium=8, large=10):
@@ -30,3 +35,38 @@ def getColorMap(fig, range, orient="horizontal", num_points=100):
     plt.colorbar(orientation=orient)
     plt.tight_layout()
     return fig
+
+
+def animate():
+    trajNc = "../../results/temp/traj.nc"
+    ps.init()
+    prevFrame = 0
+    currFrame = 0
+    isPlay = False
+    transparency = 0.4
+    maxFrame = dg_read.sizeOf(trajNc) - 1
+    face, vertex = dg_read.readMeshByNc(trajNc, currFrame)
+    mesh = ps.register_surface_mesh("mesh", vertex, face)
+    ps.set_transparency_mode("pretty")
+
+    def callback():
+        psim.PushItemWidth(100)
+        nonlocal prevFrame, currFrame, isPlay, face, vertex, mesh, transparency
+        changed, currFrame = psim.SliderInt("frame", currFrame, 0, maxFrame)
+        changed, transparency = psim.SliderFloat(
+            "transparency", transparency, 0, 1)
+        if psim.Button("Play/Pause"):
+            isPlay = ~isPlay
+        if (prevFrame != currFrame | changed):
+            face, vertex = dg_read.readMeshByNc(trajNc, currFrame)
+            mesh = ps.register_surface_mesh(
+                "mesh", vertex, face, transparency=transparency
+            )
+        if isPlay:
+            currFrame = currFrame + 1
+            if currFrame >= maxFrame:
+                currFrame = 0
+
+    ps.set_up_dir("z_up")
+    ps.set_user_callback(callback)
+    ps.show()
