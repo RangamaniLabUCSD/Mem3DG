165cdcca3ba026135e5b52fed4f9b9f1f0da1d04
cuzhucuncheng <cuzhu@ucsd.edu>
2022-09-22 10:38:04 -0700

    fix: over small increment led to issue in floating point comparison

--- src/solver/integrator/velocity_verlet.cpp
+++ src/solver/integrator/velocity_verlet.cpp
@@ -97,7 +97,7 @@ bool VelocityVerlet::integrate() {
 
     // step forward
     if (system.time == lastProcessMesh || system.time == lastUpdateGeodesics) {
-      system.time += 1e-10 * characteristicTimeStep;
+      system.time += 1e-5 * characteristicTimeStep;
     } else {
       march();
     }
