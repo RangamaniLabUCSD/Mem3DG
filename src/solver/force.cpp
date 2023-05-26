08cb4959c66844e8392f074752c6a90b70f00a60
Christopher T. Lee <ctlee@ucsd.edu>
2023-05-26 09:54:11 -0700

    refactor: add protection in case omp is not installed
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>

--- src/solver/force.cpp
+++ src/solver/force.cpp
@@ -12,7 +12,12 @@
 //     Padmini Rangamani (prangamani@eng.ucsd.edu)
 //
 
+#ifdef _OPENMP
 #include <omp.h>
+#else
+#define omp_get_num_threads() 0
+#define omp_get_thread_num() 0
+#endif
 
 #include "mem3dg/solver/system.h"
 
