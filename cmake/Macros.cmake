f133362301f86e4c3bd5f7db412e2ce7d18fde84
Christopher T. Lee <ctlee@ucsd.edu>
2024-01-24 13:35:54 -0800

    build: Updating versioning to use PKG-INFO or git_archival
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -22,60 +22,59 @@ Macro sets the following variables
 macro(get_version_from_git)
   # Look for version from GIT
   include(GetGitRevisionDescription)
-  git_describe_working_tree(VERSION --tags --always)
+  git_describe_working_tree(VERSION)
 
-  # parse the version information into pieces.
-  string(REGEX MATCH
-      "^v?(([0-9]+)(\\.?[0-9]+)*)-?(alpha[0-9]*|beta[0-9]*|dev[0-9]*|rc[0-9]*|a[0-9]*|b[0-9]*|c[0-9]*)?-?([0-9]+)?-?([a-z0-9]+)?-?(dirty)?$"
+  if(VERSION MATCHES [[.*NOTFOUND]])
+    # else() Check for PKG-INFO from sdist
+    if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/PKG-INFO)
+      file(STRINGS ${CMAKE_CURRENT_SOURCE_DIR}/PKG-INFO VERSION
+           REGEX "^Version: (.*)$"
+      )
+      string(REPLACE "Version: " "" VERSION ${VERSION})
+    elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/.git_archival.txt)
+      file(STRINGS ${CMAKE_CURRENT_SOURCE_DIR}/.git_archival.txt VERSION
+           REGEX "^describe-name: (.*)$"
+      )
+      string(REPLACE "describe-name: " "" VERSION ${VERSION})
+    endif()
+  endif()
+  message(STATUS ${VERSION})
+  string(
+    REGEX
+      MATCH
+      [[^v?([0-9]+(\.[0-9]+)*)[_.-]?(alpha[0-9]*|beta[0-9]*|dev[0-9]*|rc[0-9]*|a[0-9]*|b[0-9]*|c[0-9]*)?[_.-]?d?e?v?([0-9]+)?[_.+-]?([a-z0-9]+)?[_.-]?(d[0-9]+)?-?(dirty)?$]]
       MATCH_RESULT
       "${VERSION}"
   )
   if(MATCH_RESULT)
-    # foreach(_TMP RANGE 10)
-    #   message(STATUS "MATCH ${_TMP}: ${CMAKE_MATCH_${_TMP}}")
-    # endforeach()
+    foreach(_TMP RANGE 10)
+      message(TRACE "MATCH ${_TMP}: ${CMAKE_MATCH_${_TMP}}")
+    endforeach()
     set(VERSION_SHORT ${CMAKE_MATCH_1})
-    set(VERSION_INFO ${CMAKE_MATCH_4})
+    set(VERSION_INFO ${CMAKE_MATCH_3})
     set(VERSION_SHA1 ${CMAKE_MATCH_5})
-    set(VERSION_DIRTY ${CMAKE_MATCH_6})
 
-    set(VERSION_DUMP "${VERSION_SHORT}")
-    if(VERSION_INFO)
-      string(CONCAT VERSION_DUMP ${VERSION_DUMP} "-${VERSION_INFO}")
-    endif()
-    configure_file(
-      ${CMAKE_CURRENT_SOURCE_DIR}/cmake/VERSION.in
-      ${CMAKE_CURRENT_SOURCE_DIR}/VERSION
-    )
   else()
-    message(STATUS "No GIT VCS found pulling version from file")
-    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION VERSION)
-    string(STRIP "${VERSION}" VERSION)
-    string(REGEX MATCH
-        "^v?(([0-9]+)(\\.?[0-9]+)*)-?(alpha[0-9]*|beta[0-9]*|dev[0-9]*|a[0-9]*|b[0-9]*|c[0-9]*)?$"
-        MATCH_RESULT
-        "${VERSION}"
-    )
-    # foreach(_TMP RANGE 9)
-    #   message(STATUS "MATCH ${_TMP}: ${CMAKE_MATCH_${_TMP}}")
-    # endforeach()
-    if(MATCH_RESULT)
-      set(VERSION_SHORT ${CMAKE_MATCH_1})
-      if(CMAKE_MATCH_4)
-        set(VERSION_INFO ${CMAKE_MATCH_4})
-      endif()
-    endif()
+    message(WARNING "Version could not be determined")
+    set(VERSION_SHORT "0.0.0")
+    set(VERSION_INFO "")
+    set(VERSION_SHA1 "")
   endif()
-
 endmacro(get_version_from_git)
-
 
 macro(print_debug_messages LANG)
   message(DEBUG "CMAKE_${LANG}_FLAGS is: ${CMAKE_${LANG}_FLAGS}")
   message(DEBUG "CMAKE_${LANG}_FLAGS_DEBUG is: ${CMAKE_${LANG}_FLAGS_DEBUG}")
-  message(DEBUG "CMAKE_${LANG}_FLAGS_RELEASE is: ${CMAKE_${LANG}_FLAGS_RELEASE}")
-  message(DEBUG "CMAKE_${LANG}_FLAGS_RELWITHDEBINFO is: ${CMAKE_${LANG}_FLAGS_RELWITHDEBINFO}")
-  message(DEBUG "CMAKE_${LANG}_FLAGS_MINSIZEREL is: ${CMAKE_${LANG}_FLAGS_MINSIZEREL}")
+  message(DEBUG
+          "CMAKE_${LANG}_FLAGS_RELEASE is: ${CMAKE_${LANG}_FLAGS_RELEASE}"
+  )
+  message(
+    DEBUG
+    "CMAKE_${LANG}_FLAGS_RELWITHDEBINFO is: ${CMAKE_${LANG}_FLAGS_RELWITHDEBINFO}"
+  )
+  message(DEBUG
+          "CMAKE_${LANG}_FLAGS_MINSIZEREL is: ${CMAKE_${LANG}_FLAGS_MINSIZEREL}"
+  )
   message(DEBUG "Build type: ${CMAKE_BUILD_TYPE}")
   message(DEBUG "CMAKE_VERBOSE_MAKEFILE: " ${CMAKE_VERBOSE_MAKEFILE})
   message(DEBUG "BUILD_SHARED_LIBS: " ${BUILD_SHARED_LIBS})
